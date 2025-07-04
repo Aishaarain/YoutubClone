abde4f9b11bb3444ddc60936adea964e2d30b400
Aishaarain <155973754+Aishaarain@users.noreply.github.com>
2025-07-04 23:37:46 +0500

    Update WatchPage.jsx

--- src/pages/WatchPage.jsx
+++ src/pages/WatchPage.jsx
@@ -15,7 +15,7 @@ const video = location.state?.video;
           />
       </div>
       <h1 className="text-2xl font-bold mb-2">Video Title Placeholder</h1>
-      <p className="text-gray-700">Channel Name • 100K views • 2 days ago</p>
+      <p className="text-gray-700">Channel Name . 100K views . 2 days ago</p>
     </div>
   );
 }
